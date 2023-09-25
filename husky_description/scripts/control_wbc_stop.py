67a573dd20d07483de9b5a5e4f20669eb8bb98e3
FrankSLaubach <laubach.f@northeastern.edu>
2023-09-25 12:35:02 -0400

    git fix 2

--- husky_description/scripts/control_wbc_stop.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-
-import rosservice
-import time
-
-if __name__ == '__main__':
-    time.sleep(1)
-    rosservice.call_service("/controller_manager/switch_controller",\
-        [[''],['controllers/legged_controller'],1,True, 0.0])
